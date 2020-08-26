740ae1bf99e5f6af7c57a3e07072750dd7bc02ad
Evan <zhangxiaogang@weihongtech.cn>
2020-08-26 22:09:23 +0800

    微信支付

--- foodie-shop/js/main.js
+++ foodie-shop/js/main.js
@@ -1,7 +1,7 @@
 //创建全局GoEasy对象
 Vue.prototype.$goEasy = new GoEasy({
     host:'hangzhou.goeasy.io', //应用所在的区域地址: 【hangzhou.goeasy.io |singapore.goeasy.io】
-    appkey: "BC-7e86cb1cbc6e4381b5deb649d3261103", //替换为您的应用appkey
+    appkey: "替换为您的应用appkey", //替换为您的应用appkey
     onConnected: function() {
         console.log('连接成功！')
     },
